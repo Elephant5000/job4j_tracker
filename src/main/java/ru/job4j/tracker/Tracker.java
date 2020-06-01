313283ea44d080097c73a6125ff2c08b965e228d
Elephant5000 <pashau@mail.ru>
2020-06-01 17:03:56 +0300

    1.2.3.5. Tracker - хранилище [#294114] Tracker.java

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,23 +1,27 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
-    private int size = 0;
+    private int position = 0;
 
     public Item add(Item item) {
         item.setId(generateId());
-        items[size++] = item;
+        items[position++] = item;
         return item;
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
+        for (int index = 0; index < position; index++) {
             Item item = items[index];
             if (item.getId().equals(id)) {
                 rsl = item;
@@ -25,5 +29,30 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+    public Item[] findAll(){
+        Item[] itemsWithoutNul = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                itemsWithoutNul[size] = items[index];
+                size++;
+            }
+        }
+        itemsWithoutNul = Arrays.copyOf(itemsWithoutNul, size);
+        return itemsWithoutNul;
+    }
+
+    public Item[] findByName(String key){
+        Item[] itemFindName = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getName().equals(key)) {
+                itemFindName[size] = items[index];
+                size++;
+            }
+        }
+        itemFindName = Arrays.copyOf(itemFindName,size);
+        return itemFindName;
     }
 }
