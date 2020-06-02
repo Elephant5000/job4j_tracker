fd043260ebe5b974f8933e4220d7b160a48202d9
Elephant5000 <pashau@mail.ru>
2020-06-02 15:58:24 +0300

    1.2.3.7. Метод удаления заявки Tracker.delete [#294112]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 
 public class TrackerTest {
     @Test
@@ -28,5 +29,15 @@ public class TrackerTest {
 
         Boolean result = tracker.replace(idItemForReplace, item3);
         assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDelete() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        tracker.add(bug);
+        String id = bug.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id), is(nullValue()));
     }
 }
