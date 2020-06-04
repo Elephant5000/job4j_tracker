7e485241a76dc9d4ee3eb33ec508b0efcbd6190f
Elephant5000 <pashau@mail.ru>
2020-06-04 09:03:06 +0300

    1.2.4.3.1. Приведение типов. Повышение типа и понижение типа. [#294051]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        /*
+        * Приведение up casting
+         */
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = freshman;
+
+    }
+}
