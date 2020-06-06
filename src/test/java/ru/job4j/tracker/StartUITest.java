d5e7b260cf421d0f54ec1c289934eb6362923c62
Elephant5000 <pashau@mail.ru>
2020-06-06 08:42:48 +0300

    1.2.4.9. Написать тесты на StartUI. [#294058]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -18,7 +18,7 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
-
+/*
     @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
@@ -40,4 +40,56 @@ public class StartUITest {
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
     }
+
+
+ */
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        /* Добавим в tracker новую заявку */
+        Item item = tracker.add(new Item("Replaced item"));
+        String idReplaced = item.getId();
+        String replacedName = "New item name";
+        Input in = new StubInput(
+                new String[] {"0", idReplaced, replacedName, "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        /* Добавим в tracker новую заявку */
+        Item item = tracker.add(new Item("Deleted item"));
+        String deleteId = item.getId();
+        Input in = new StubInput(
+                new String[] {"0", deleteId, "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertNull(tracker.findById(item.getId()));
+    }
+
 }
