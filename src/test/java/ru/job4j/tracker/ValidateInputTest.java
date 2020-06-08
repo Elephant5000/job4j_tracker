fddc17b40d21db61e6770192b612fb5b020d901d
Elephant5000 <pashau@mail.ru>
2020-06-08 20:55:51 +0300

    1.2.5.2. Рефакторинг - Шаблон Декоратор для валидатора. [#294062] дополнения

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -27,4 +27,16 @@ public class ValidateInputTest {
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(3));
     }
+
+    @Test
+    public void whenNumberFormatException() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(out.toString(), is("Please enter validate data again.\n"));
+    }
+
 }
