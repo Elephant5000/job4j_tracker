ba4db6657e3897f6ea182c0a83d218cd017fb43b
Elephant5000 <pashau@mail.ru>
2020-06-19 01:18:19 +0300

    1.3.2.2. Идентичные тексты. [#294039]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<String>();
+        // for-each origin -> new HashSet.
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String word : text) {
+            rsl = check.contains(word);
+            if (!rsl)  {
+                break;
+            }
+        }
+        // for-each text -> hashSet.contains
+        return rsl;
+    }
+}
