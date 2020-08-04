1a7fd375043f8819770e02d9f71bae08eefdba18
Elephant5000 <pashau@mail.ru>
2020-08-04 21:16:27 +0300

    1.4.2.1. Фильтрация учеников. [#293888]

---
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SchoolTest {
+
+    @Test
+    public void When70to100collect() {
+        List<Student> studentList = List.of(
+                new Student("Ivanov Ivan", 80),
+                new Student("Petrov Petr", 90),
+                new Student("Ivanova Ivanka", 69),
+                new Student("Tretyakova Aglaya", 40)
+        );
+        List<Student> expect = List.of(
+                new Student("Ivanov Ivan", 80),
+                new Student("Petrov Petr", 90)
+        );
+        List<Student> result = School.collect(studentList, student -> student.getScore()>70);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void When50to70collect() {
+        List<Student> studentList = List.of(
+                new Student("Ivanov Ivan", 80),
+                new Student("Petrov Petr", 90),
+                new Student("Ivanova Ivanka", 69),
+                new Student("Tretyakova Aglaya", 40)
+        );
+        List<Student> expect = List.of(
+                new Student("Ivanova Ivanka", 69)
+        );
+        List<Student> result = School.collect(studentList, student -> student.getScore()>50 && student.getScore()<=70);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void When0to50collect() {
+        List<Student> studentList = List.of(
+                new Student("Ivanov Ivan", 80),
+                new Student("Petrov Petr", 90),
+                new Student("Ivanova Ivanka", 69),
+                new Student("Tretyakova Aglaya", 40)
+        );
+        List<Student> expect = List.of(
+                new Student("Tretyakova Aglaya", 40)
+        );
+        List<Student> result = School.collect(studentList, student -> student.getScore()<=50);
+        assertThat(result, is(expect));
+    }
+
+}
