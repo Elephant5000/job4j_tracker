1ec9f93bc92a28a233ddd58f955a123b12e3bbe3
Elephant5000 <pashau@mail.ru>
2020-08-11 19:04:11 +0300

    1.4.2.4. Преобразование List в Map. [#293891] part 2

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -1,12 +1,10 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ListToMap {
     public static void main(String[] args) {
-        //List <Student> studentList = new ArrayList<>();
         List<Student> studentList = List.of(
                 new Student("Ivanov Ivan", 80),
                 new Student("Petrov Petr", 90),
@@ -15,13 +13,13 @@ public class ListToMap {
                 new Student("Tretyakova Aglaya", 40)
         );
 
-        System.out.println(studentList);
-
         System.out.println(
-                studentList.stream().distinct().collect(Collectors.toMap(
-                e -> e.getSurname(),
-                e -> e
-        ))
+                studentList.stream()
+                        .collect(Collectors.toMap(
+                                Student::getSurname,
+                                e -> e,
+                                (oldValue, newValue) -> newValue
+                        ))
         );
     }
 }
