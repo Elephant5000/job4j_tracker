0368b4ab2a3e2dc1e10825380baf899e3440a2f1
Elephant5000 <pashau@mail.ru>
2020-08-14 11:03:24 +0300

    1.3.5.0. Stream API улучшения [#293897] рефакторинг

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -8,9 +8,8 @@ import java.util.stream.Stream;
 public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
-                //.flatMap(Stream::ofNullable) в уроке выше так убираются NULL
-                .filter(st -> st != null) // но раз в задании через Фильтр, то так
-                .sorted((left, right) -> left.getScore() - right.getScore())
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> right.getScore() - left.getScore())
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
